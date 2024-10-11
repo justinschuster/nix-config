28d5b3f75d94c7588a435ae18327f2360bd58c9c
justinschuster <schujustin@gmail.com>
2024-10-11 02:37:28 -0400

    reduced number of nerdfonts

--- services/system.nix
+++ services/system.nix
@@ -25,6 +25,34 @@
 
   services.printing.enable = true;
 
+  fonts = {
+    packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+
+      # normal fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+
+      # nerdfonts
+      (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
+    ];
+
+    # use fonts specified by user rather than default ones
+    enableDefaultPackages = false;
+
+    # user defined fonts
+    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+    # B&W emojis that would sometimes show instead of some Color emojis
+    fontconfig.defaultFonts = {
+      serif = ["Noto Serif" "Noto Color Emoji"];
+      sansSerif = ["Noto Sans" "Noto Color Emoji"];
+      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+      emoji = ["Noto Color Emoji"];
+    };
+  };
+
  # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -32,8 +60,8 @@
     curl
     gcc
     git
-    gnumake 
-    nerdfonts
+    gnumake
+    just
     vim
     wget
     xclip
